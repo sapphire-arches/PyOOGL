ba2fc6fd86bf91b988270abc1b20e9ae1b1acbc7
bobtwinkles <srkoser+GitHub@gmail.com>
2014-07-15 18:38:51 -0400

    add some more simple drawing functions

--- simple/geometry.py
+++ simple/geometry.py
@@ -1,5 +1,4 @@
-from OpenGL.GL import glBegin, glEnd, glVertex2f, glColor3f, glGetError
-from OpenGL.GL import GL_QUADS, GL_TRIANGLE_FAN
+from OpenGL.GL import *
 from math import pi, cos, sin
 
 def draw_circle(x, y, radius, color, verts=32):
@@ -12,4 +11,20 @@ def draw_circle(x, y, radius, color, verts=32):
         glVertex2f(x + radius * cos(angle), y + radius * sin(angle))
         angle += 2 * pi / verts
     glVertex2f(x + radius, y)
+    glEnd()
+
+def draw_rectangle(x, y, width, height, color):
+    glBegin(GL_QUADS)
+    glColor3f(color[0], color[1], color[2])
+    glVertex2f(x, y)
+    glVertex2f(x + width, y)
+    glVertex2f(x + width, y + height)
+    glVertex2f(x, y + height)
+    glEnd()
+
+def draw_line(x1, y1, x2, y2, color):
+    glBegin(GL_LINE)
+    glColor3f(color[0], color[1], color[2])
+    glVertex2f(x1, y1)
+    glVertex2f(x2, y2)
     glEnd()
