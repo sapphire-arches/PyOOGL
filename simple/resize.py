4af1c1239fcd2ac6ed6448439636dc907ef678c8
bobtwinkles <srkoser+GitHub@gmail.com>
2014-07-20 22:26:13 -0400

    viewport transforms are Important

--- simple/resize.py
+++ simple/resize.py
@@ -1,7 +1,8 @@
-from OpenGL.GL import glLoadIdentity, glScalef, glTranslatef
+from OpenGL.GL import glViewport, glLoadIdentity, glScalef, glTranslatef
 # approximate SDL resize behavior (1:1 units to pixels + top left is 0, 0)
 def sdl_resize():
     def resize(w, h):
+        glViewport(0, 0, w, h)
         glLoadIdentity()
         glTranslatef(-1 , 1, 0)
         glScalef(2 / w, -2 / h, 1)
