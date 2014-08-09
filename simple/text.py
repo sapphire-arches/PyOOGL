a81862f614470b338159c4369b035451f69f578f
addtheletters <bzhang.2003@gmail.com>
2014-08-08 23:49:55 -0400

    Text: Font and encoding args
    also specified imports

--- simple/text.py
+++ simple/text.py
@@ -1,7 +1,29 @@
-from OpenGL.GLUT import *
-from OpenGL.GL   import *
+from OpenGL.GLUT import  glutBitmapString,\
+                         GLUT_BITMAP_8_BY_13,\
+                         GLUT_BITMAP_9_BY_15,\
+                         GLUT_BITMAP_TIMES_ROMAN_10,\
+                         GLUT_BITMAP_TIMES_ROMAN_24,\
+                         GLUT_BITMAP_HELVETICA_10,\
+                         GLUT_BITMAP_HELVETICA_12,\
+                         GLUT_BITMAP_HELVETICA_18
+                         
+from OpenGL.GL   import glRasterPos, glColor3f
 
-def draw_text(text, x, y, color):
+def draw_text(text, x, y, color, font = GLUT_BITMAP_HELVETICA_18, enc = 'ascii'):
     glRasterPos(x, y)
     glColor3f(color[0], color[1], color[2])
-    glutBitmapString(GLUT_BITMAP_HELVETICA_18, text.encode('ascii'))
+    glutBitmapString(font, text.encode(enc))
+
+# Maybe have a way to resize text?
+# Dunno how hard that would be.
+
+bitmap_font = {"8x13" : GLUT_BITMAP_8_BY_13,
+         "9x15" : GLUT_BITMAP_9_BY_15,
+         "tnr": GLUT_BITMAP_TIMES_ROMAN_10,
+         "tnr10": GLUT_BITMAP_TIMES_ROMAN_10,
+         "tnr24": GLUT_BITMAP_TIMES_ROMAN_24,
+         "helvetica": GLUT_BITMAP_HELVETICA_12,
+         "helvetica10": GLUT_BITMAP_HELVETICA_10,
+         "helvetica12": GLUT_BITMAP_HELVETICA_12,
+         "helvetica18": GLUT_BITMAP_HELVETICA_18,
+    }
